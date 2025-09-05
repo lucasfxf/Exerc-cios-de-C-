415f29778586bfcecc9ed0c566939029564ac46f
lucasfxf <lucastrabalho171088@gmail.com>
2025-09-05 19:08:35 -0300

    Add files via upload
    atividade 11 string.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite a mensagem codificada:");
+        string mensagemCodificada = Console.ReadLine()!;
+
+        string mensagemDecodificada = DecodificarMensagem(mensagemCodificada);
+
+        Console.WriteLine("Mensagem decodificada:");
+        Console.WriteLine(mensagemDecodificada);
+    }
+
+    static string DecodificarMensagem(string codificada)
+    {
+        string resultado = "";
+        
+        for (int i = 0; i < codificada.Length; i++)
+        {
+            if (codificada[i] == 'p' || codificada[i] == 'P')
+            {
+            
+                if (i + 1 < codificada.Length)
+                {
+                    resultado += codificada[i + 1];
+                    i++; 
+                }
+            }
+            else
+            {
+                resultado += codificada[i];
+            }
+        }
+
+        return resultado;
+    }
+}
