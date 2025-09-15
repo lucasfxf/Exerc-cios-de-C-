65d4d467091bb28061bf3f137c76c78a9096791f
lucasfxf <lucastrabalho171088@gmail.com>
2025-09-14 21:12:29 -0300

    Add files via upload
    Ex01 matriz

---
+++ Ex05.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Ex05
+{
+    // Função que retorna o menor valor da matriz
+    static int MenorValor(int[,] matriz)
+    {
+        int menor = matriz[0, 0];
+        for (int i = 0; i < matriz.GetLength(0); i++) // linhas
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++) // colunas
+            {
+                if (matriz[i, j] < menor)
+                {
+                    menor = matriz[i, j];
+                }
+            }
+        }
+        return menor;
+    }
+
+    static void Main()
+    {
+        int[,] matriz = {
+            { 5, 2, 8 },
+            { 7, 2, 9 },
+            { 4, 2, 1 }
+        };
+
+        Console.WriteLine("Menor valor da matriz: " + MenorValor(matriz));
+    }
+}
